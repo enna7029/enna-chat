9de60f3732f74197bc18466ef8ca3fa8a8a0ba57
王龙 <w1069103107@outlook.com>
2024-08-01 11:27:53 +0800

    web进程,websocket进程,register进程,任务处理进程设置完毕

--- src/start_businessworker.php
+++ src/start_businessworker.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace Enna\Chat;
+
+use Workerman\Worker;
+use GatewayWorker\BusinessWorker;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$worker = new BusinessWorker();
+
+$worker->name = 'ChatBusinessWorker';
+
+$worker->count = 4;
+
+$worker->registerAddress = '127.0.0.1:1236';
+
+if (!defined('GLOBAL_START')) {
+    Worker::runAll();
+}
--- src/start_gateway.php
+++ src/start_gateway.php
@@ -1,1 +1,29 @@
 <?php
+
+namespace Enna\Chat;
+
+use Workerman\Worker;
+use GatewayWorker\Gateway;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$gateway = new Gateway("websocket://0.0.0.0:7272");
+
+$gateway->name = 'ChatGateway';
+
+$gateway->count = 2;
+
+$gateway->lanIp = '127.0.0.1';
+
+$gateway->startPort = 2300;
+
+$gateway->pingInterval = 10;
+
+$gateway->pingData = '{"type":"ping"}';
+
+$gateway->registerAddress = '127.0.0.1:1236';
+
+if (!defined('GLOBAL_START')) {
+    Worker::runAll();
+}
+
